209d5fef915622dbedf8cdbe2f0df1c13a100061
mstfrs <mstfrs68@gmail.com>
2022-02-02 22:01:44 +0300

    secret key

--- weather/settings.py
+++ weather/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-k^edwz*fy&%0ukw4^-@4v&jh^p(8(dpgpu#%wbur)@x19i=@&z'
+
 
 SECRET_KEY = config("SECRET_KEY")
 
